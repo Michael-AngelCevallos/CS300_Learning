1cc0784164d6725de97dda0fb3931372be464357
Michael-AngelCevallos <118484898+Michael-AngelCevallos@users.noreply.github.com>
2026-05-08 06:48:58 -0500

    Finished Array Example

---
+++ arrayPractice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arrayList1[4] = {1, 2, 3, 4};
+
+    cout << arrayList1[0] << endl; // OUTPUTS THE FIRST ELEMENT OF THE ARRAY, WHICH IS 1
+    cout << arrayList1[1] << endl; // OUTPUTS THE SECOND ELEMENT OF THE ARRAY, WHICH IS 2
+    cout << arrayList1[2] << endl; // OUTPUTS THE THIRD ELEMENT OF THE ARRAY, WHICH IS 3
+    cout << arrayList1[3] << endl; // OUTPUTS THE FOURTH ELEMENT OF THE ARRAY, WHICH IS 4
+
+    return 0;
+}
