ad6d6f6cddf91fd54a484f0c3da01b9ccf33ca82
Michael-AngelCevallos <118484898+Michael-AngelCevallos@users.noreply.github.com>
2026-05-08 06:16:00 -0500

    Added Variables lesson for integrers

--- arrayTest.cpp
+++ arrayTest.cpp
@@ -4,6 +4,6 @@ using namespace std;
 main()
 {
     int score[4] = {90, 70, 120, 80};
-    cout << score[1] << endl;
+    cout << score[1] << endl; // OUTPUTS THE SECOND ELEMENT OF THE ARRAY, WHICH IS 70
     return 0;
 }
