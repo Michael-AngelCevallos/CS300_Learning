04e1ce6c705e38c05a07cc03dcd60a191d877204
Michael-AngelCevallos <118484898+Michael-AngelCevallos@users.noreply.github.com>
2026-05-08 05:24:25 -0500

    Array test in C++

---
+++ arrayTest.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int score[4] = {90, 70, 120, 80};
+    cout << score[1] << endl;
+    return 0;
+}
--- basicOutputStructure.cpp
+++ basicOutputStructure.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-using namespace std;
+#include <iostream>  // GIVES INPUT\OUTPUT FEATURES
+using namespace std; // ALLOWS US TO USE THE STANDARD NAMESPACE, SO WE DON'T HAVE TO PREFIX STANDARD LIBRARY NAMES WITH "std::"
 
-int main()
+int main() // `
 {
     int x = 5;
     int y = 7;
